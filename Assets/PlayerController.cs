805528b80aec15a9da253ff59b338fa8c18f1381
Hui Yoo <huiyoo@umich.edu>
2020-08-03 13:33:30 -0400

    basic movement and rotation

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity) 
+    {
+        velocity = _velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        
+        transform.LookAt(lookPoint);
+    }
+
+    // Update is called once per frame
+    public void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }   
+}
---
+++ Assets/WeaponController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform weaponHold;
+    public Weapon startingWeapon;
+    Weapon currentWeapon;
+    void Start()
+    {
+        currentWeapon = Instantiate(startingWeapon, weaponHold.position, weaponHold.rotation);
+        currentWeapon.transform.parent = weaponHold;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
