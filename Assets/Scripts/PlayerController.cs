dd5b2a814ee06c939cdcc23e7a993b080875ffec
Xilg <32823215+Xilg@users.noreply.github.com>
2020-08-03 14:12:21 -0400

    DashBasicMechanic
    Notfinished, basic dash mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,15 @@ public class PlayerController : MonoBehaviour
 
     Vector3 velocity;
     Rigidbody myRigidbody;
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private int direction;
+
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
+        dashTime = startDashTime;
     }
 
     public void Move(Vector3 _velocity) 
@@ -27,6 +33,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     public void FixedUpdate()
     {
+        if(direction == 0)
+        {
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                direction = 1;
+            }
+            else if(Input.GetKeyDown(KeyCode.D))
+            {
+                direction = 2;
+            }
+            else if(Input.GetKeyDown(KeyCode.W))
+            {
+                direction = 3;
+            }
+            else if(Input.GetKeyDown(KeyCode.S))
+            {
+                direction = 4;
+            }
+            else
+            {
+                dashTime -= Time.deltaTime;
+                if(direction == 1)
+                {
+                    myRigidbody.velocity = Vector2.left * dashSpeed;
+                }
+            }
+        }
         myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
     }   
 }
