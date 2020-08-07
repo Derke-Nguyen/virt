4fa405b3e63b6f6dc92ac58fccd6a5572ff8c22e
siweisun0118 <50390650+siweisun0118@users.noreply.github.com>
2020-08-07 10:37:54 -0400

    Player healthbar no longer rotates

---
+++ Assets/Camera/PlayerHealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.back, mainCamera.transform.rotation * Vector3.up);
+    }
+}
