c767f80f68b16d9e7ba2a230e86b7516d7772d9f
siweisun0118 <50390650+siweisun0118@users.noreply.github.com>
2020-08-06 23:04:44 -0400

    Manual Merge with Hui's AI
    MOSTLY WORKS

---
+++ Assets/Scripts/Enemy1 States/Enemy1BaseState.cs
@@ -0,0 +1,16 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+
+//Abstract class definition which all other states inherit from
+public abstract class Enemy1BaseState
+{
+    public abstract void EnterState(Enemy1 enemy);
+
+    public abstract void Update(Enemy1 enemy);
+
+    public abstract void OnCollisionEnter(Enemy1 enemy);
+
+    public abstract void FixedStateUpdate(Enemy1 enemy);
+}
