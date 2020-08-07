5f0cc4b080be040475485b1bbb9614d14a60fee7
siweisun0118 <50390650+siweisun0118@users.noreply.github.com>
2020-08-07 17:53:03 -0400

    Stuff and Fixes and Stuffs

---
+++ Assets/Scripts/SetupScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupScript : MonoBehaviour
+{
+    GameObject[] Enemies;
+
+    public Transform here;
+    public GameObject QuestItem;
+
+    bool generated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if (Enemies.Length == 0 && !generated)
+        {
+            generated = true;
+            Debug.Log("All Enemies Defeated");
+            Instantiate(QuestItem, here.position, here.rotation);
+        }
+    }
+}
