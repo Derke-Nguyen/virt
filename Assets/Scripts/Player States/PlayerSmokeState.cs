d3307a897fd5185cb2304e355e4c2802d7d8d408
RichardCanXu <richardcanxu@gmail.com>
2020-08-08 19:13:48 -0400

    smoke state with comments
    added comments

--- Assets/Scripts/Player States/PlayerSmokeState.cs
+++ Assets/Scripts/Player States/PlayerSmokeState.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class PlayerSmokeState : PlayerBaseState
 {
+    //smokeprefab
     public GameObject SmokePrefab;
 
     public override void EnterState(PlayerController player)
     {
+        //gets smokeprefab from playercontroller
         SmokePrefab = player.GetComponent<PlayerController>().SmokePrefab;
 
+        //gets position of player
         Vector3 spawnsmokeloc = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
 
+        //clones prefab and instatiates it
         GameObject clone = Object.Instantiate(SmokePrefab, spawnsmokeloc, Quaternion.identity);
-        Object.Destroy(clone, 2.0f);
-        player.TransitionToState(player.IdleState);
+        Object.Destroy(clone, 2.0f);//destorys it
+        player.TransitionToState(player.IdleState);//goes back to idle
     }
 
-    //Nothing should go here
+    //Nothing goes here
     public override void FixedStateUpdate(PlayerController player)
     {
 
     }
 
+    //pretty sure nothing goes here
     public override void OnCollisionEnter(PlayerController player)
     {
 
