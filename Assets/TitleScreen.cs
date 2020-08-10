c4bc505dae8b297098281eb78f33c2bcb1ea0868
Philip <pgk1216@gmail.com>
2020-08-09 21:26:46 -0400

    titlescreenv1

---
+++ Assets/TitleScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+
+    public int optionsSceneIndex;
+
+    public void Play() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Options() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + optionsSceneIndex);
+    }
+
+    public void Quit() {
+        Debug.Log("quited");
+        Application.Quit();
+    }
+}
