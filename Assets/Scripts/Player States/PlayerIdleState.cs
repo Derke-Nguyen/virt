376eac42d07fee677bb6fe79eeaf3373c3a7c9cd
siweisun0118 <50390650+siweisun0118@users.noreply.github.com>
2020-08-15 03:15:24 -0400

    Lowered melee attack CD

--- Assets/Scripts/Player States/PlayerIdleState.cs
+++ Assets/Scripts/Player States/PlayerIdleState.cs
@@ -7,7 +7,7 @@ public class PlayerIdleState : PlayerBaseState
 
 
     public static float dashCD_value = 1;
-    public static float swingCD_value = 0.7f;
+    public static float swingCD_value = 0.5f;
 
     public static float dashCD = 0; //Global variable since Idle state and Jump state should share the same dash cooldown
     public static float swingCD = 0; //Global variable since Idle state and Dash state should share the same dash coodown
